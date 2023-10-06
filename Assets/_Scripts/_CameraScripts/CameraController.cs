d71419c01f77740ef13e40176d8c059a38d00cd6
Alchemane <kevsaxton7@gmail.com>
2023-10-06 19:37:51 +0100

    more work on camera

--- Assets/_Scripts/_CameraScripts/CameraController.cs
+++ Assets/_Scripts/_CameraScripts/CameraController.cs
@@ -7,9 +7,11 @@ namespace GameRoot.InGame.Navigation.CameraNavigation
 {
     public class CameraController : MonoBehaviour
     {
-        public Transform followTransform;
+        public Transform followTransform; // wip
         public Transform cameraTransform;
+        public GameObject ground;
 
+        // public inspector fields
         public float normalSpeed;
         public float fastSpeed;
         public float movementSpeed;
@@ -17,14 +19,16 @@ namespace GameRoot.InGame.Navigation.CameraNavigation
         public float rotationAmount;
         public Vector3 zoomAmount;
 
-        public Vector3 newPosition;
-        public Quaternion newRotation;
-        public Vector3 newZoom;
+        private Vector3 newPosition;
+        private Quaternion newRotation;
+        private Vector3 newZoom;
+        private Vector3 dragStartPosition;
+        private Vector3 dragCurrentPosition;
+        private Vector3 rotateStartPosition;
+        private Vector3 rotateCurrentPosition;
 
-        public Vector3 dragStartPosition;
-        public Vector3 dragCurrentPosition;
-        public Vector3 rotateStartPosition;
-        public Vector3 rotateCurrentPosition;
+        private float minX, maxX, minZ, maxZ; // map boundaries
+        private float minZoomX, maxZoomX, minZoomY, maxZoomY, minZoomZ, maxZoomZ; // zoom limits
 
         // Start is called before the first frame update
         void Start()
@@ -32,6 +36,23 @@ namespace GameRoot.InGame.Navigation.CameraNavigation
             newPosition = transform.position;
             newRotation = transform.rotation;
             newZoom = cameraTransform.localPosition;
+
+            // get the size of the ground object
+            Vector3 groundSize = ground.GetComponent<Renderer>().bounds.size;
+
+            // set the boundaries based on the ground size
+            minX = ground.transform.position.x - groundSize.x / 2;
+            maxX = ground.transform.position.x + groundSize.x / 2;
+            minZ = ground.transform.position.z - groundSize.z / 2;
+            maxZ = ground.transform.position.z + groundSize.z / 2;
+
+            // set the zoom limits based on the ground size
+            minZoomX = groundSize.x * 0.1f; // 10%
+            maxZoomX = groundSize.x * 0.9f; // 90%
+            minZoomZ = groundSize.z * 0.1f;
+            maxZoomZ = groundSize.z * 0.9f;
+            minZoomY = groundSize.x * 0.1f;
+            maxZoomY = groundSize.x * 0.5f;
         }
 
         // Update is called once per frame
